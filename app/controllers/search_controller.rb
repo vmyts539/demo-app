efa523a00e3fd32aa8220874e1af10e60cb6ed83
Vasyl Mytskaniuk <vasul237125@gmail.com>
2020-05-14 09:51:44 +0300

    add loader, rework search procces, remove jquery,

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,18 +5,30 @@ class SearchController < ApplicationController
       match: :word_start,
              misspellings: { edit_distance: 0 }
 
-    @projects = Project.search params[:search],
-      fields: ["name^10", "description"],
-      match: :word_start,
-             misspellings: { edit_distance: 0 }
+    # @projects = Project.search params[:search],
+    #   fields: ["name^10", "description"],
+    #   match: :word_start,
+    #          misspellings: { edit_distance: 0 }
 
+    sleep(2)
     respond_to do |format|
       format.html
       format.json
    end
   end
+
+  def autocomplete
+    render json: User.search(params[:search], {
+      fields: [:first_name, :last_name, :full_name],
+      match: :word_start,
+      misspellings: { edit_distance: 0 },
+      limit: 7
+    }).map(&:full_name)
+  end
 end
 
-# 1. Move search to react
-# 2. Add  panel
-# 3. Highlighting
+# add highliting
+# autocomplete
+# redux
+# push to heroku
+# add elastic specs
--- app/javascript/components/Search/Search.jsx
+++ app/javascript/components/Search/Search.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import axios from 'axios';
 import { Link } from "react-router-dom";
+import Loader from 'react-loader-spinner'
 
-import $ from 'jquery';
-import 'jquery-ui/ui/widgets/autocomplete';
 
 import './search.scss';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 class Search extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { loading: false };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.inputChange = this.inputChange.bind(this);
+    this.setUsers = this.setUsers.bind(this);
+    this.renderUser = this.renderUser.bind(this);
+    this.setLoading = this.setLoading.bind(this);
   };
 
+  setUsers(users) { this.setState({users: users}) }
+  setLoading(bool) { this.setState({loading: bool}) }
+
   getUser(keyword) {
-    const self = this;
+    this.setLoading(true)
 
     axios({
       url: '/search?search=' + keyword,
@@ -25,31 +31,44 @@ class Search extends React.Component {
       content_type: 'application/json',
       headers: { 'Accept': 'application/json'},
     })
-    .then(function (response) {
-      self.setState({data: response.data.data.users});
+    .then((response) => {
+      this.setUsers(response.data.data.users);
+      this.setLoading(false);
     })
-    .catch(function (response) {
+    .catch((response) => {
+      console.log(response.errors.message);
     });
   }
 
   inputChange(event) {
     this.setState({keyword: event.target.value});
-
-    let autocompletion = [];
-    console.log(event.target.value);
-    this.getUser(event.target.value);
-    this.state.data && this.state.data.map(user => {
-      autocompletion.push(user.first_name + ' ' + user.last_name);
-    })
-
-    $("#search").autocomplete({source: autocompletion});
+    // this.getUser(event.target.value);
+    // this.state.data && this.state.data.map(user => {
+    //   // autocomplete logic
+    // })
   }
 
   handleSubmit(event) {
     event.preventDefault();
     this.getUser(this.state.keyword);
-    this.setState({users: this.state.data});
   };
+
+  renderUser(user) {
+    return(
+      <div className='user w-50 mt-3' key={user.id}>
+        <div className="full-name text-center p-3">{user.first_name} {user.last_name}</div>
+
+        <div className="info d-flex justify-content-between align-items-start">
+          <div className="email"><span>Email:</span> {user.email}</div>
+          <div className="phone"><span>Phone:</span> {user.phone}</div>
+        </div>
+
+        <Link to={"/users/" + user.id}>
+          <button className="btn btn-primary mt-3">Show</button>
+        </Link>
+      </div>
+    )
+  }
 
   render () {
     return (
@@ -82,33 +101,29 @@ class Search extends React.Component {
             </div>
           </div>
         </form>
+        <div className="loader-wrapper">
+            {
+              this.state.loading &&
+              <div className="loader">
+                <Loader
+                  type="Oval"
+                  color="#00BFFF"
+                  height={100}
+                  width={100}
+                />
+              </div>
+            }
 
-        <div className="results d-flex flex-column align-items-center">
-          {
-            this.state.users &&
-            this.state.users.map(user => <User key={user.id} user={user} />)
-          }
+          <div className="results d-flex flex-column align-items-center">
+            {
+              this.state.users &&
+              this.state.users.map(user => this.renderUser(user))
+            }
+          </div>
         </div>
       </div>
     )
   }
-}
-
-const User = ({user}) => {
-  return (
-    <div className='user w-50 mt-3'>
-      <div className="full-name text-center p-3">{user.first_name} {user.last_name}</div>
-
-      <div className="info d-flex justify-content-between align-items-start">
-        <div className="email"><span>Email:</span> {user.email}</div>
-        <div className="phone"><span>Phone:</span> {user.phone}</div>
-      </div>
-
-      <Link to={"/users/" + user.id}>
-        <button className="btn btn-primary mt-3">Show</button>
-      </Link>
-    </div>
-  )
 }
 
 export default Search
